a521c766745bd251416b90ccf67c6a03637e609a
Mr.PAPI <thesnakewitcher@gmail.com>
2023-11-24 12:46:50 -0500

    change diagnostics and signs

--- lua/signs.lua
+++ lua/signs.lua
@@ -1,7 +1,11 @@
+---@doc {diagnostic}
+vim.fn.sign_define("DiagnosticSignError", {text = "", texthl = "DiagnosticSignError"})
+vim.fn.sign_define("DiagnosticSignWarn", {text = "", texthl = "DiagnosticSignWarn"})
+vim.fn.sign_define("DiagnosticSignInfo", {text = "", texthl = "DiagnosticSignInfo"})
+vim.fn.sign_define("DiagnosticSignHint", {text = "󰌵", texthl = "DiagnosticSignHint"})
+
+
+---@doc {signs}
 vim.fn.sign_define("DapBreakpoint", {text = "", texthl = "DapBreakpoint"})
 vim.fn.sign_define("DapBreakpointCondition", {text = "", texthl = "DapBreakpointCondition"})
 vim.fn.sign_define("DapBreakpointRejected", {text = "", texthl = "DapBreakpointRejected"})
-
--- DapLogPoint = ".>",
--- DapStopped = "",
--- DefaultFile = "",
