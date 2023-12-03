7221a020a06d89f4251e17a838ae1f5056fa39ea
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-03 13:19:31 -0500

    adjust require snipperts in lua snippets

--- luasnippets/lua.lua
+++ luasnippets/lua.lua
@@ -21,35 +21,49 @@ local postfix = require("luasnip.extras.postfix").postfix
 
 ls.add_snippets("lua", {
 
---------------------------
--- codebase structure
---------------------------
-s({
-    name = "require",
-    trig = "require",
-    dscr = "function declaration",
-}, fmt([[
-    {}{} = require("{}")
-]], {
-    c(1, {
-        t("local "),
-        t(""),
-    }),
-    i(2, "name"),
-    i(3, "module")
-})),
+    --------------------------
+    -- codebase structure
+    --------------------------
+    s({
+        name = "require",
+        trig = "require",
+        dscr = "function declaration",
+    }, fmt([[
+        {}{} = require("{}")
+    ]], {
+        c(1, {
+            t("local "),
+            t(""),
+        }),
+        d(3,function(args)
+            local module_path = vim.split(args[1][1], '.', true)
+            return sn(
+                nil,
+                i(1, module_path[#module_path] or "")
+            )
+        end,{2},{}),
+        i(2, "module")
+    })),
 
-s(
-    {
+    s({
+        name = "return",
+        trig = "return",
+        dscr = "return statement",
+    }, fmt([[
+        return {}
+    ]], {
+        i(1)
+    })),
+
+    s({
         name = "function",
         trig = "fn",
         dscr = "function declaration",
-        -- snippetType = "autosnippet",
     }, fmt([[
-        {} {} = function({})
+        {}function {}({})
             {}
         end
-    ]],{
+    ]], {
         c(1, {
             t("local "),
             t("")
@@ -63,177 +77,143 @@ s(
             t("")
         }),
         i(4, "-- code --")
-    })
-),
+    })),
 
     --------------------------
     -- control structure
     --------------------------
-s(
-    {
+    s({
         name = "if",
         trig = "if",
         dscr = "if statement declaration",
-    },
-    fmt([[
+    },fmt([[
         if ({}) then
             {}
         end
     ]], {
-    i(1, "cond"),
-    i(2, "code"),
-    })
-),
+        i(1, "cond"),
+        i(2, "code"),
+    })),
 
-    s(
-            {
-                    name = "if",
-                    trig = "ife",
-                    dscr = "if/else statement declaration",
-            },
-            fmt([[
+    s({
+        name = "if",
+        trig = "ife",
+        dscr = "if/else statement declaration",
+    },fmt([[
         if ({}) then
             {}
         else
             {}
         end
-    ]]       , {
-                    i(1, "cond"),
-                    i(2, "code"),
-                    i(3, "code"),
-            })
-    ),
+    ]], {
+        i(1, "cond"),
+        i(2, "code"),
+        i(3, "code"),
+    })),
 
-    s(
-            {
-                    name = "for",
-                    trig = "forr",
-                    dscr = "for statement declaration",
-            },
-            fmt([[
+    s({
+        name = "for",
+        trig = "forr",
+        dscr = "for statement declaration",
+    },fmt([[
         for {} = {} , {}{} do
             {}
         end
-    ]]       , {
-                    i(1, "var"),
-                    i(2, "start"),
-                    i(3, "end"),
-                    c(4, {
-                            sn(nil, {
-                                    t(" , "),
-                                    i(1, "step"),
-                            }),
-                            t(""),
-                    }),
-                    i(5, "-- code --"),
-            })
-    ),
+    ]], {
+        i(1, "var"),
+        i(2, "start"),
+        i(3, "end"),
+        c(4, {
+            sn(nil, {
+                t(" , "),
+                i(1, "step"),
+            }),
+            t(""),
+        }),
+        i(5, "-- code --"),
+    })),
 
-    s(
-            {
-                    name = "while",
-                    trig = "while",
-                    dscr = "execute code while condition is true",
-            },
-            fmt([[
+    s({
+        name = "while",
+        trig = "while",
+        dscr = "execute code while condition is true",
+    },fmt([[
         while {} do
             {}
         end
-    ]]       , {
-                    i(1, "condition"),
-                    i(2, "-- code --"),
-            })
-    ),
+    ]], {
+        i(1, "condition"),
+        i(2, "-- code --"),
+    })),
 
-    s(
-            {
-                    name = "repeat until",
-                    trig = "repeat",
-                    dscr = "repeat code until condition became true",
-            },
-            fmt([[
+    s({
+        name = "repeat until",
+        trig = "repeat",
+        dscr = "repeat code until condition became true",
+    }, fmt([[
         repeat
             {}
         until {}
-    ]]       , {
-                    i(2, "-- code --"),
-                    i(1, "condition"),
-            })
-    ),
+    ]], {
+        i(2, "-- code --"),
+        i(1, "condition"),
+    })),
+
     --------------------------
     -- data structure
     --------------------------
-    s(
-            {
-                    name = "types",
-                    trig = "types",
-                    dscr = "types",
-            },
-            fmt([[
+    s({
+        name = "types",
+        trig = "types",
+        dscr = "types",
+    },fmt([[
         {}
-    ]]       , {
-                    c(1, {
-                            t("string"),
-                            t("number"),
-                            t("bool"),
-                            t("nil"),
-                    }),
-            })
-    ),
+    ]], {
+        c(1, {
+            t("string"),
+            t("number"),
+            t("bool"),
+            t("nil"),
+        }),
+    })),
 
-    s(
-            {
-                    name = "local",
-                    trig = "local",
-                    dscr = "variable declaration statement declaration",
-            },
-            fmt([[
-        for {} = {} , {}{} do
-            {}
-        end
-    ]]       , {
-                    i(1, "var"),
-                    i(2, "start"),
-                    i(3, "end"),
-                    c(4, {
-                            sn(nil, {
-                                    t(" , "),
-                                    i(1, "step"),
-                            }),
-                            t(""),
-                    }),
-                    i(5, "-- code --"),
-            })
-    ),
+    s({
+        name = "local",
+        trig = "local",
+        dscr = "variable declaration statement declaration",
+    }, fmt([[
+        local {} = {}
+    ]], {
+        i(1, "var"),
+        i(2, "value"),
+    })),
 
     --------------------------
     -- luasnip
     --------------------------
-    s(
-            {
-                    name = "snippet",
-                    trig = "snippet",
-                    dscr = "luasnip snippet declaration",
-            },
-            fmt([[
-  s(
-    {{
-        name = "{}",
-        trig = "{}",
-        dscr = "{}",
-    }},
-    fmt([[
-        {}
-    ] ],{{
-        {}
-    }}),
-  ),
-]]           , {
-                    i(1, "name"),
-                    i(2, "trig"),
-                    i(3, "dscr"),
-                    i(4, "code"),
-                    i(5, "nodes"),
-            })
-    ),
+    s({
+        name = "snippet",
+        trig = "snippet",
+        dscr = "luasnip snippet declaration",
+    }, fmt([[
+      s(
+        {{
+            name = "{}",
+            trig = "{}",
+            dscr = "{}",
+        }},
+        fmt([[
+            {}
+        ] ],{{
+            {}
+        }}),
+      ),
+    ]], {
+        i(1, "name"),
+        i(2, "trig"),
+        i(3, "dscr"),
+        i(4, "code"),
+        i(5, "nodes"),
+    })),
+
 })
