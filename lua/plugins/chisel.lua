3e16c436cdcc18375b5fdc3b523253d8a520d14b
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-02 22:45:56 -0500

    remove unneeded plugins config

--- lua/plugins/chisel.lua
+++
@@ -1,8 +0,0 @@
-local ok, chisel = pcall(require,"chisel")
-if not ok then
-    vim.notify "chisel config not loaded"
-    return
-end
-
-
-chisel.setup()
--- lua/plugins/forge.lua
+++
@@ -1,8 +0,0 @@
-local ok, forge = pcall(require,"forge")
-if not ok then
-    vim.notify "forge config not loaded"
-    return
-end
-
-
-forge.setup()
