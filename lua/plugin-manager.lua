663864656d363628a3b486f1da1bc3ab383f6bef
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-10 15:20:57 -0500

    add plugin-manager

--- lua/plugin-manager.lua
+++ lua/plugin-manager.lua
@@ -1,6 +1,5 @@
 -- TODO: move cmds/autocmds to after/
 -- TODO: enable spell in specific filetypes and comments
--- TODO: search where is seted <leader>D definition keymap and remove it
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
