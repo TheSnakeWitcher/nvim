ded29be46768e65902da7597230595ea81f10385
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-24 09:41:11 -0500

    reorder config to load cmds and autocmds last

--- init.lua
+++ init.lua
@@ -4,14 +4,14 @@ for _, module in ipairs({
     "options",     --- @help {options}
     "plugins",
     "keymap",      --- @help {lua-keymap}
-    "autocmds",    --- @help {lua-guide-autocommands}
-    "cmds",        --- @help {lua-guide-commands}
     "diagnostics", --- @help {diagnostic.txt}
     "signs",       --- @help {sign.txt}
-    "highlight",
+    "highlight",   --- @help {highlight}
+    "autocmds",    --- @help {lua-guide-autocommands}
+    "cmds",        --- @help {lua-guide-commands}
 }) do
-  local ok, _ = pcall(require, module)
-  if not ok then
+    local ok, _ = pcall(require, module)
+    if not ok then
         vim.notify("failed to load module " .. module)
     end
 end
