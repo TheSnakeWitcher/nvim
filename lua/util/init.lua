8044ed304223415c6a27ce5906eab5b38abaeaff
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-23 19:26:05 -0500

    add load_config to util modules

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -1,6 +1,15 @@
 util = {}
 
 
+function util.load_config(module)
+    local ok, module_config = pcall(require, "plugins." .. module)
+    if not ok then
+        vim.notify(module .. " config not loaded")
+        return {}
+    end
+    return module_config
+end
+
 util.ui = require("util.ui")
 util.icons = require("util.icons")
 
