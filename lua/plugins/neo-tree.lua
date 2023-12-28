96accdd35cfaa6e5b262d757eb39b03e433dbe8c
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-28 13:10:19 -0500

    update neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,4 +1,4 @@
-local ok, neo_tree = pcall(require,"neo-tree")
+local ok, neo_tree = pcall(require, "neo-tree")
 if not ok then
     vim.notify "neo-tree config not loaded"
     return
@@ -7,21 +7,21 @@ end
 --- @help {neo-tree-configuration}
 neo_tree.setup({
     source_selector = {
-        winbar = false,
+        winbar = true,
         statusline = false,
         show_scrolled_off_parent_node = false,
         sources = {
             {
                 source = "filesystem",
-                display_name = "  Files "
+                display_name = " 󰈙 Files "
             },
             {
                 source = "buffers",
-                display_name = "  Buffers "
+                display_name = "  Buffers "
             },
             {
                 source = "git_status",
-                display_name = "  Git "
+                display_name = " 󰊢 Git "
             },
         },
         content_layout = "start",
@@ -30,7 +30,7 @@ neo_tree.setup({
         tabs_min_width = nil,
         tabs_max_width = nil,
         padding = 0,
-        separator = { left = "▏", right= "▕" },
+        separator = { left = "▏", right = "▕" },
         separator_active = nil,
         show_separator_on_edge = false,
         highlight_tab = "NeoTreeTabInactive",
@@ -40,25 +40,18 @@ neo_tree.setup({
         highlight_separator_active = "NeoTreeTabSeparatorActive",
     },
 
-        close_if_last_window = false,
-        popup_border_style = "rounded",
-        enable_git_status = true,
-        enable_diagnostics = true,
-        sort_case_insensitive = false,
-        sort_function = nil,
-        -- sort_function = function (a,b)
-        --       if a.type == b.type then
-        --           return a.path > b.path
-        --       else
-        --           return a.type > b.type
-        --       end
-        --   end , -- this sorts files and directories descendantly
+    close_if_last_window = false,
+    popup_border_style = "rounded",
+    enable_git_status = true,
+    enable_diagnostics = true,
+    sort_case_insensitive = false,
+    sort_function = nil,
 
-        default_component_configs = {
-          container = {
+    default_component_configs = {
+        container = {
             enable_character_fade = true
-          },
-          indent = {
+        },
+        indent = {
             indent_size = 2,
             padding = 1,
 
@@ -71,45 +64,45 @@ neo_tree.setup({
             expander_collapsed = "",
             expander_expanded = "",
             expander_highlight = "NeoTreeExpander",
-          },
-          icon = {
+        },
+        icon = {
             folder_closed = "",
             folder_open = "",
             folder_empty = "",
             default = "*",
             highlight = "NeoTreeFileIcon"
-          },
-          modified = {
+        },
+        modified = {
             symbol = "",
             highlight = "NeoTreeModified",
-          },
-          name = {
+        },
+        name = {
             trailing_slash = false,
             use_git_status_colors = true,
             highlight = "NeoTreeFileName",
-          },
-          git_status = {
-            symbols = {
-              added     = "",
-              modified  = "",
-              deleted   = "",
-              renamed   = "➜",
-              untracked = "",
-              ignored   = "◌",
-              unstaged  = "✗",
-              staged    = "✓",
-              conflict  = "",
-            }
-          },
         },
-        windo = {
-          position = "left",
-          width = 40,
-          mapping_options = {
+        git_status = {
+            symbols = {
+                added     = "",
+                modified  = "",
+                deleted   = "",
+                renamed   = "➜",
+                untracked = "",
+                ignored   = "◌",
+                unstaged  = "✗",
+                staged    = "✓",
+                conflict  = "",
+            }
+        },
+    },
+    windo = {
+        position = "left",
+        width = 40,
+        mapping_options = {
             noremap = true,
             nowait = true,
-          },
-          mappings = {
+        },
+        mappings = {
             ["<space>"] = {
                 "toggle_node",
                 nowait = false,
@@ -132,10 +125,10 @@ neo_tree.setup({
             ["z"] = "close_all_nodes",
             --["Z"] = "expand_all_nodes",
             ["a"] = {
-              "add",
-              config = {
-                show_path = "none"
-              }
+                "add",
+                config = {
+                    show_path = "none"
+                }
             },
             ["A"] = "add_directory",
             ["d"] = "delete",
@@ -150,11 +143,11 @@ neo_tree.setup({
             ["?"] = "show_help",
             ["<"] = "prev_source",
             [">"] = "next_source",
-          }
-        },
-        nesting_rules = {},
-        filesystem = {
-          filtered_items = {
+        }
+    },
+    nesting_rules = {},
+    filesystem = {
+        filtered_items = {
             visible = false,
             hide_dotfiles = true,
             hide_gitignored = true,
@@ -164,50 +157,50 @@ neo_tree.setup({
             always_show = {},
             never_show = {},
             never_show_by_pattern = {},
-          },
-          follow_current_file = true,
-          group_empty_dirs = false,
-          hijack_netrw_behavior = "open_default",
-          use_libuv_file_watcher = true,
-          window = {
-            mappings = {
-              ["<bs>"] = "navigate_up",
-              ["."] = "set_root",
-              ["H"] = "toggle_hidden",
-              ["/"] = "fuzzy_finder",
-              ["D"] = "fuzzy_finder_directory",
-              ["f"] = "filter_on_submit",
-              ["<c-x>"] = "clear_filter",
-              ["[g"] = "prev_git_modified",
-              ["]g"] = "next_git_modified",
-            }
-          }
         },
-        buffers = {
-          follow_current_file = true,
-          group_empty_dirs = true,
-          show_unloaded = true,
-          window = {
+        follow_current_file = true,
+        group_empty_dirs = false,
+        hijack_netrw_behavior = "open_default",
+        use_libuv_file_watcher = true,
+        window = {
             mappings = {
-              ["bd"] = "buffer_delete",
-              ["<bs>"] = "navigate_up",
-              ["."] = "set_root",
+                ["<bs>"] = "navigate_up",
+                ["."] = "set_root",
+                ["H"] = "toggle_hidden",
+                ["/"] = "fuzzy_finder",
+                ["D"] = "fuzzy_finder_directory",
+                ["f"] = "filter_on_submit",
+                ["<c-x>"] = "clear_filter",
+                ["[g"] = "prev_git_modified",
+                ["]g"] = "next_git_modified",
             }
-          },
+        }
+    },
+    buffers = {
+        follow_current_file = true,
+        group_empty_dirs = true,
+        show_unloaded = true,
+        window = {
+            mappings = {
+                ["bd"] = "buffer_delete",
+                ["<bs>"] = "navigate_up",
+                ["."] = "set_root",
+            }
         },
-        git_status = {
-          window = {
+    },
+    git_status = {
+        window = {
             position = "float",
             mappings = {
-              ["A"]  = "git_add_all",
-              ["gu"] = "git_unstage_file",
-              ["ga"] = "git_add_file",
-              ["gr"] = "git_revert_file",
-              ["gc"] = "git_commit",
-              ["gp"] = "git_push",
-              ["gg"] = "git_commit_and_push",
+                ["A"]  = "git_add_all",
+                ["gu"] = "git_unstage_file",
+                ["ga"] = "git_add_file",
+                ["gr"] = "git_revert_file",
+                ["gc"] = "git_commit",
+                ["gp"] = "git_push",
+                ["gg"] = "git_commit_and_push",
             }
-          }
         }
+    }
 
 })
