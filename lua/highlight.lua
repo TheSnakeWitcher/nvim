3bbc90aff8fc29cce6e074f1b0051adf97fed4ae
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-28 13:14:31 -0500

    add lualine highlight

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -1,4 +1,10 @@
--- see :hi :highlight , nvim_set_hl , nvim_get_hl
--- colors: #ecbe7b #c678dd
+--- @help :highlight , nvim_set_hl , nvim_get_hl
+
+-- vimwiki
 vim.cmd("highlight tklink ctermfg=72 guifg=#ecbe7b cterm=bold,underline gui=bold,underline")
 vim.cmd("highlight link tkTag xmlTag")
+
+
+ -- lualine
+vim.cmd("hi! link lualine_c_normal Normal")
+vim.cmd("hi! link lualine_c_inactive Normal")
