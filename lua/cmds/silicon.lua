38d92083895060524130294f2430eec6b089f37e
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-28 17:29:16 -0500

    remove screenshot command using cli tools

--- lua/cmds/silicon.lua
+++ lua/cmds/silicon.lua
@@ -3,7 +3,7 @@ if not ok then
     return
 end
 
---- @doc {silicon.lua-usage}
+--- @help {silicon.lua-usage}
 vim.api.nvim_create_user_command("Screenshot", function(opts)
     if opts.range ~= 0 then
 	    silicon.visualise_cmdline({})
@@ -14,22 +14,3 @@ end, {
 	desc = "Generate image of lines in a visual selection if range provided else from buffer",
 	range = "%",
 })
-
--- vim.api.nvim_create_user_command("ScreenshotPic", function()
---     local slurp , grim = "slurp" , "grim"
---
---     if not vim.fn.executable(slurp) or not vim.fn.executable(grim) then
---         vim.notify("missed " .. slurp .. " or " .. grim )
---         return
---     end
---
---     local buf , date = vim.fn.expand("%:t:r") , os.date()
---     local path = vim.env.HOME .. "/Pictures/Screenshots"
---     local cmd = string.format("%s | %s %s/%s-%s",slurp,grim,path,buf,date)
---     vim.notify(cmd)
---     vim.fn.system(cmd)
---     -- os.execute(cmd)
---
--- end, {
--- 	desc = "Select with cursor an screen are to take a screenshot",
--- })
