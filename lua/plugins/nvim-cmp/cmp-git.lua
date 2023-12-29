99b6aded40bd2faa2be0b9a078851e6ab054846d
Mr.PAPI <thesnakewitcher@gmail.com>
2023-12-29 18:20:15 -0500

    setup cmp-git source

--- lua/plugins/nvim-cmp/cmp-git.lua
+++ lua/plugins/nvim-cmp/cmp-git.lua
@@ -10,116 +10,17 @@ if not ok then
     return
 end
 
-local format = require("cmp_git.format")
-local sort = require("cmp_git.sort")
-
 
 --- @help {cmp-git-config}
 cmp_git.setup({
-    filetypes = { "gitcommit", "octo" },
-    remotes = { "upstream", "origin" },
-    enableRemoteUrlRewrites = false,
-    git = {
-        commits = {
-            limit = 100,
-            sort_by = sort.git.commits,
-            format = format.git.commits,
-        },
-    },
-    github = {
-        hosts = {},
-        issues = {
-            fields = { "title", "number", "body", "updatedAt", "state" },
-            filter = "all",
-            limit = 100,
-            state = "open",
-            sort_by = sort.github.issues,
-            format = format.github.issues,
-        },
-        mentions = {
-            limit = 100,
-            sort_by = sort.github.mentions,
-            format = format.github.mentions,
-        },
-        pull_requests = {
-            fields = { "title", "number", "body", "updatedAt", "state" },
-            limit = 100,
-            state = "open",
-            sort_by = sort.github.pull_requests,
-            format = format.github.pull_requests,
-        },
-    },
-    gitlab = {
-        hosts = {},
-        issues = {
-            limit = 100,
-            state = "opened",
-            sort_by = sort.gitlab.issues,
-            format = format.gitlab.issues,
-        },
-        mentions = {
-            limit = 100,
-            sort_by = sort.gitlab.mentions,
-            format = format.gitlab.mentions,
-        },
-        merge_requests = {
-            limit = 100,
-            state = "opened",
-            sort_by = sort.gitlab.merge_requests,
-            format = format.gitlab.merge_requests,
-        },
-    },
-    trigger_actions = {
-        {
-            debug_name = "git_commits",
-            trigger_character = ":",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.git:get_commits(callback, params, trigger_char)
-            end,
-        },
-        {
-            debug_name = "gitlab_issues",
-            trigger_character = "#",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.gitlab:get_issues(callback, git_info, trigger_char)
-            end,
-        },
-        {
-            debug_name = "gitlab_mentions",
-            trigger_character = "@",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.gitlab:get_mentions(callback, git_info, trigger_char)
-            end,
-        },
-        {
-            debug_name = "gitlab_mrs",
-            trigger_character = "!",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.gitlab:get_merge_requests(callback, git_info, trigger_char)
-            end,
-        },
-        {
-            debug_name = "github_issues_and_pr",
-            trigger_character = "#",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.github:get_issues_and_prs(callback, git_info, trigger_char)
-            end,
-        },
-        {
-            debug_name = "github_mentions",
-            trigger_character = "@",
-            action = function(sources, trigger_char, callback, params, git_info)
-                return sources.github:get_mentions(callback, git_info, trigger_char)
-            end,
-        },
-    },
-  }
-)
+    filetypes = { "gitcommit", "octo", "git" },
+    remotes = { "origin", "upstream" },
+})
 
+--- @help {cmp.setup.filetype}
 cmp.setup.filetype({'gitcommit','octo'}, {
     sources = cmp.config.sources({
         { name = 'git' },
-    }, {
         { name = 'buffer' },
     })
 })
