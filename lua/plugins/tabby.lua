1e5c0b77e250bedbf4f5d2c68c61f5428f70cc8c
Mr.PAPI <thesnakewitcher@gmail.com>
2024-01-01 18:24:54 -0500

    remove icon from tabs in tabby config

--- lua/plugins/tabby.lua
+++ lua/plugins/tabby.lua
@@ -23,7 +23,6 @@ tabline.set(function(line) return {
 		local hl = tab.is_current() and theme.current_tab or theme.tab
 		return {
 			line.sep("", hl, theme.fill),
-			tab.is_current() and '' or '',
 			tab.number(),
 			tab.name(),
 			tab.close_btn(""),
