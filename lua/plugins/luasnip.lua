0963cc04ea0a33826bafc9cd7dde1079f4146b13
Mr.PAPI <thesnakewitcher@gmail.com>
2024-01-04 13:07:25 -0500

    remove unneeded comments in luasnip config

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,43 +1,28 @@
-local status_ok , ls = pcall(require,"luasnip")
-if not status_ok then
+local ok , ls = pcall(require,"luasnip")
+if not ok then
     vim.notify("luasnip config submodules not loaded")
     return
 end
 
-
-local status_ok , types = pcall(require, "luasnip.util.types")
-if not status_ok then
-    vim.notify("luasnip.util.types config submodule not loaded")
-    return
-end
-
-
-require("luasnip.loaders.from_lua").lazy_load({
-    path = vim.g.snippets_dir
-    -- path = vim.g.path.snippets
-})
-
-
-ls.filetype_extend("typescriptreact", { "typescript" })
+local types = require("luasnip.util.types")
+local lua_loader = require("luasnip.loaders.from_lua") --- @help {luasnip-loaders}
 
 
 --- @help {luasnip-config-options}
 ls.config.set_config({
-    history = true,
+
     enable_autosnippets = true ,
     update_events = "TextChanged,TextChangedI",
-    --delete_check_events = 'InsertLeave',
-    --ft_func = require('luasnip.extras.filetype_functions').from_cursor,
+
+    --- @help {luasnip-ext_opts}
     ext_opts = {
         [types.choiceNode] = {
           active = {
             virt_text = { { '●', 'DiagnosticHint' } },
           },
         },
-        --[types.insertNode] = {
-        --  active = {
-        --    virt_text = { { '', 'DiagnosticWarn' } },
-        --  },
-        --},
     },
 })
+
+lua_loader.lazy_load({path = vim.g.path.snippets })
+ls.filetype_extend("typescriptreact", { "typescript" })
