298f527c6a3172cf3154469f9a8fcbaa6f6f8fbd
Mr.PAPI <thesnakewitcher@gmail.com>
2024-02-10 15:51:39 -0500

    add highlight autocmd to config

--- lua/autocmds/config.lua
+++ lua/autocmds/config.lua
@@ -36,3 +36,13 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
         vim.opt_local.spell = true
     end,
 })
+
+
+local highlight = vim.api.nvim_create_augroup("HighlightYank", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "highlight yanked text",
+    group = highlight,
+    pattern = "*",
+    callback = function() vim.highlight.on_yank() end,
+})
