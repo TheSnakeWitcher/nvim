76b0d4f95afd23ee03d95f85eeff0b0353cd4359
Mr.PAPI <thesnakewitcher@gmail.com>
2024-03-04 11:23:26 -0500

    add markdown ftplugin comment cmd

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,6 +1,8 @@
 -- vim.cmd("autocmd! BufEnter * if &ft ==# 'markdown' | wincmd L | endif")
 vim.api.nvim_cmd({ cmd = 'wincmd', args = { 'L' } }, {})
 
+-- vim.cmd("autocmd FileType markdown setlocal spell")
+
 vim.api.nvim_buf_set_keymap(
     vim.api.nvim_get_current_buf(),
     "i","[[","<esc><cmd>Telekasten insert_link<cr>",
