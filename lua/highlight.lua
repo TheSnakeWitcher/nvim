1b38cb5d218f330a4880e742116cc3bac7622de7
Mr.PAPI <thesnakewitcher@gmail.com>
2024-04-12 00:39:47 -0400

    remove vimwiki highlights

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -1,9 +1,5 @@
 --- @help :highlight , nvim_set_hl , nvim_get_hl
 
--- vimwiki
-vim.cmd("highlight tklink ctermfg=72 guifg=#ecbe7b cterm=bold,underline gui=bold,underline")
-vim.cmd("highlight link tkTag xmlTag")
-
 
  -- lualine
 vim.cmd("hi! link lualine_c_normal Normal")
