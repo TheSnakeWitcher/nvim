64e1934b8b336d7514a19292c36c16a8851a6640
Mr.PAPI <thesnakewitcher@gmail.com>
2024-04-12 00:37:55 -0400

    add obsidian.nvim config

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,42 @@
+local ok, obsidian = pcall(require, "obsidian")
+if not ok then
+    vim.notify "obsidian config not loaded"
+    return
+end
+
+
+--- @help {obsidian-setup}
+obsidian.setup({
+    workspaces = {
+        { name = "zettelkasten", path = vim.g.path.knowledgebase .. "/zettelkasten" },
+        { name = "wiki",         path = vim.g.path.knowledgebase .. "/wiki" },
+    },
+    ui = {
+        enable = true,
+        update_debounce = 200,
+        checkboxes = {
+            [" "] = { char = "󰄱 ", hl_group = "ObsidianTodo" },
+            ["x"] = { char = " ", hl_group = "ObsidianDone" },
+            [">"] = { char = " ", hl_group = "ObsidianRightArrow" },
+            ["~"] = { char = "󰰱 ", hl_group = "ObsidianTilde" },
+        },
+        bullets = { char = "•", hl_group = "ObsidianBullet" },
+        external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+        reference_text = { hl_group = "ObsidianRefText" },
+        highlight_text = { hl_group = "ObsidianHighlightText" },
+        tags = { hl_group = "ObsidianTag" },
+        block_ids = { hl_group = "ObsidianBlockID" },
+        hl_groups = {
+            ObsidianTodo = { bold = true, fg = "#f78c6c" },
+            ObsidianDone = { bold = true, fg = "#89ddff" },
+            ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+            ObsidianTilde = { bold = true, fg = "#ff5370" },
+            ObsidianBullet = { bold = true, fg = "#89ddff" },
+            ObsidianRefText = { underline = true, fg = "#c792ea" },
+            ObsidianExtLinkIcon = { fg = "#c792ea" },
+            ObsidianTag = { italic = true, fg = "#89ddff" }, -- "#ecbe7b"
+            ObsidianBlockID = { italic = true, fg = "#89ddff" },
+            ObsidianHighlightText = { bg = "#75662e" },
+        },
+    },
+})
