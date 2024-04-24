c87ceb446f5aaed23f44ab206afeba6fe3cda473
Mr.PAPI <thesnakewitcher@gmail.com>
2024-04-23 23:22:09 -0400

    remove telekasten keymap from markdown ftplugin

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,9 +2,3 @@
 vim.api.nvim_cmd({ cmd = 'wincmd', args = { 'L' } }, {})
 
 -- vim.cmd("autocmd FileType markdown setlocal spell")
-
-vim.api.nvim_buf_set_keymap(
-    vim.api.nvim_get_current_buf(),
-    "i","[[","<esc><cmd>Telekasten insert_link<cr>",
-    { desc = "insert link" }
-)
