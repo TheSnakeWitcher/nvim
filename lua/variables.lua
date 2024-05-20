6af44e18268ced58e141dcf69ce807f4683aaf63
Mr.PAPI <thesnakewitcher@gmail.com>
2024-05-20 11:26:56 -0400

    create vim.g.path

--- lua/variables.lua
+++ lua/variables.lua
@@ -4,13 +4,6 @@
 -- user variables
 --------------------------------------------------------------
 vim.g.mapleader = " "
-vim.g.knowledgebase_dir = vim.env.HOME .. "/Knowledgebase"
-vim.g.code_dir = vim.env.HOME .. "/Code"
-vim.g.scripts_dir = vim.g.code_dir .. "/scripts"
-vim.g.projects_dir = vim.g.code_dir .. "/projects"
-vim.g.work_projects_dir = vim.g.code_dir .. "/work"
-vim.g.plugin_dev_dir = vim.g.projects_dir .. "/nvim"
-vim.g.snippets_dir = vim.fn.stdpath("config") .. "/luasnippets"
 
 local code_dir = vim.env.HOME .. "/Code"
 local projects_dir = code_dir .. "/projects"
@@ -30,24 +23,6 @@ vim.g.path = {
 --------------------------------------------------------------
 -- vim.g.neovide_transparency = 0.9
 -- vim.g.neovide_cursor_vfx_mode = "sonicboom"
-
-
---------------------------------------------------------------
--- vimwiki
---------------------------------------------------------------
-vim.g.vimwiki_list = {
-    {
-        path =  vim.g.knowledgebase_dir .. "/wiki",
-        syntax = 'markdown',
-        ext = '.md'
-    },
-}
-vim.g.vimwiki_global_ext = 0
-vim.g.vimwiki_ext2syntax = {
-    [".md"] = "markdown",
-    [".markdown"] = "markdown",
-    ["mdown"] = "markdown",
-}
 
 
 --------------------------------------------------------------
