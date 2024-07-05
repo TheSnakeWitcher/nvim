32c22e8c45dc3ca4a37e063d825c82860098b978
Mr.PAPI <thesnakewitcher@gmail.com>
2024-07-05 12:05:46 -0300

    chore: remove default options in gitsigns config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,4 @@
-local status_ok, gitsigns = pcall(require,"gitsigns")
+local status_ok, gitsigns = pcall(require, "gitsigns")
 if not status_ok then
     vim.notify("gitsigns config not loaded")
     return
@@ -15,10 +15,6 @@ gitsigns.setup({
         topdelete    = { text = '' },
         untracked    = { text = '┆' },
     },
-    signcolumn = true,
-    numhl      = false,
-    linehl     = false,
-    word_diff  = false,
     watch_gitdir = {
         interval = 1000,
         follow_files = true
@@ -31,20 +27,6 @@ gitsigns.setup({
         delay = 1000,
         ignore_whitespace = false,
     },
-    current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> : <summary>',
-    sign_priority = 6,
-    update_debounce = 100,
-    status_formatter = nil,
-    max_file_length = 40000,
-    preview_config = {
-        border = 'single',
-        style = 'minimal',
-        relative = 'cursor',
-        row = 0,
-        col = 1,
-    },
-    yadm = {
-        enable = false
-    },
+    current_line_blame_formatter = '<author> on <author_time:%Y-%m-%d> : <summary>',
 
 })
