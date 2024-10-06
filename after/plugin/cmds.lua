04484b30c589b870ed18bda3b8608c3aaef1fccc
Mr.PAPI <thesnakewitcher@gmail.com>
2024-10-06 10:40:54 -0300

    add SwapClean user command

--- after/plugin/cmds.lua
+++ after/plugin/cmds.lua
@@ -9,3 +9,14 @@ vim.api.nvim_create_user_command(
     'lua require("toggleterm.terminal").Terminal:new({ cmd = "gh dash", direction = "float" }):toggle()<cr>',
     { desc = "open gh dash" }
 )
+
+vim.api.nvim_create_user_command(
+    "SwapClean",
+    function(opts)
+        vim.fn.jobstart(
+            string.format("rm %s",vim.fn.stdpath("state") .. "/swap/*"),
+            { on_exit = function() vim.notify("swap cleaned") end }
+        )
+    end,
+    { desc = "clean swap files" }
+)
