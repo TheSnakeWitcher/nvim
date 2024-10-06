ff85b4809073e6280c5672c260c1d09043b73901
Mr.PAPI <thesnakewitcher@gmail.com>
2024-10-06 10:39:42 -0300

    remove lua/cmds

--- lua/cmds/web3.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_user_command("WebEnableAucmd", function()
-    vim.api.nvim_get_autocmds({
-        group = "Web3",
-        event = "BufWritePost" ,
-        pattern = "*.sol"
-    })
-    --vim.api.nvim_clear_autocmds
-end, {})
