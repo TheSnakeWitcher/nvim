09110c3c0a7576c63ca42e99a30f8b3deaf7efcd
Mr.PAPI <thesnakewitcher@gmail.com>
2024-12-16 18:57:02 -0300

    add markdown syntax

--- after/syntax/markdown.lua
+++ after/syntax/markdown.lua
@@ -15,3 +15,32 @@ vim.cmd("hi RenderMarkdownH3Bg guibg=#303940")
 vim.cmd("hi RenderMarkdownH4Bg guibg=#233745")
 vim.cmd("hi RenderMarkdownH5Bg guibg=#33334e")
 vim.cmd("hi RenderMarkdownH6Bg guibg=#30304b")
+
+-- render-markdown
+vim.cmd("hi RenderMarkdownBullet guifg=#f78c6c")
+-- vim.cmd("hi linkRenderMarkdownBullet ObsidianBullet")
+
+-- bullets = { char = "•", hl_group = "ObsidianBullet" },
+-- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+-- reference_text = { hl_group = "ObsidianRefText" },
+-- highlight_text = { hl_group = "ObsidianHighlightText" },
+-- tags = { hl_group = "ObsidianTag" },
+-- block_ids = { hl_group = "ObsidianBlockID" },
+-- hl_groups = {
+--   ObsidianTodo = { bold = true, fg = "#f78c6c" },
+--   ObsidianDone = { bold = true, fg = "#89ddff" },
+--   ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+--   ObsidianTilde = { bold = true, fg = "#ff5370" },
+--   ObsidianImportant = { bold = true, fg = "#d73128" },
+--   ObsidianBullet = { bold = true, fg = "#89ddff" },
+--   ObsidianRefText = { underline = true, fg = "#c792ea" },
+--   ObsidianExtLinkIcon = { fg = "#c792ea" },
+--   ObsidianTag = { italic = true, fg = "#89ddff" },
+--   ObsidianBlockID = { italic = true, fg = "#89ddff" },
+--   ObsidianHighlightText = { bg = "#75662e" },
+
+  -- [" "] = { order = 1, char = "󰄱", hl_group = "ObsidianTodo" },
+  -- ["~"] = { order = 2, char = "󰰱", hl_group = "ObsidianTilde" },
+  -- ["!"] = { order = 3, char = "", hl_group = "ObsidianImportant" },
+  -- [">"] = { order = 4, char = "", hl_group = "ObsidianRightArrow" },
+  -- ["x"] = { order = 5, char = "", hl_group = "ObsidianDone" },
