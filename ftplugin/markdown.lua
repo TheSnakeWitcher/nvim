bf5645b3552174ec0ee9abf5ddf58a1a354dbfa6
Mr.PAPI <thesnakewitcher@gmail.com>
2025-08-06 21:21:36 -0300

    remove markdown ftplugin

--- ftplugin/markdown.lua
+++
@@ -1,4 +0,0 @@
--- vim.cmd("autocmd! BufEnter * if &ft ==# 'markdown' | wincmd L | endif")
-vim.api.nvim_cmd({ cmd = 'wincmd', args = { 'L' } }, {})
-
--- vim.cmd("autocmd FileType markdown setlocal spell")
