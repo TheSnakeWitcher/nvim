263740be994244c4c8f561803c7ba398f87330b7
Mr.PAPI <thesnakewitcher@gmail.com>
2025-08-10 15:01:59 -0300

    adjust after/plugin/autocmds

--- after/plugin/autocmds.lua
+++ after/plugin/autocmds.lua
@@ -1,23 +1,23 @@
 --- @help {lua-guide-autocommands}
 
-vim.cmd("autocmd! BufEnter * if &ft ==# 'help' | wincmd L | endif")
--- vim.cmd("autocmd! BufEnter * if &ft ==# 'markdown' | wincmd L | endif")
-
-
 local config = vim.api.nvim_create_augroup("Config", { clear = true })
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-    desc = "hot reload options",
+-- vim.cmd("autocmd! BufEnter * if &ft ==# 'help' | wincmd L | endif")
+vim.api.nvim_create_autocmd("BufEnter", {
+    desc = "open help window to the right",
     group = config,
-    pattern = vim.fn.stdpath("config") .. "/lua/options.lua",
-    command = "source <afile>",
+    callback = function()
+        if vim.bo.ft == "help" then
+            vim.cmd.wincmd("L")
+        end
+    end
 })
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-    desc = "hot reload keymap",
-    group = config,
-    pattern = vim.fn.stdpath("config") .. "/lua/keymap.lua",
-    command = "source <afile>",
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "highlight yanked text",
+    group = vim.api.nvim_create_augroup("HighlightYank", { clear = true }),
+    pattern = "*",
+    callback = function() vim.highlight.on_yank() end,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
@@ -40,28 +40,26 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "enable features supported by lsp",
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(args)
 
-local highlight = vim.api.nvim_create_augroup("HighlightYank", { clear = true })
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "highlight yanked text",
-    group = highlight,
-    pattern = "*",
-    callback = function() vim.highlight.on_yank() end,
+        -- if supported by lsp enable inlay hint
+        if client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(
+                true, -- not vim.lsp.inlay_hint.is_enabled(),
+                { bufnr = args.buf }
+            )
+        end
+
+        -- "if supported by lsp enable lsp folding",
+        if client:supports_method(vim.lsp.protocol.Methods.textDocument_foldingRange) then
+            local win = vim.api.nvim_get_current_win()
+            vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+        end
+
+    end
 })
-
--- vim.api.nvim_create_autocmd("LspAttach", {
---     group = vim.api.nvim_create_augroup("UserLspConfig", {}),
---     callback = function(args)
---
---         local client = vim.lsp.get_client_by_id(args.data.client_id)
---
---         if client.server_capabilities.inlayHintProvider then
---             vim.lsp.inlay_hint.enable(
---                 not vim.lsp.inlay_hint.is_enabled(),
---                 { bufnr = args.buf }
---             )
---         end
---
---     end
--- })
