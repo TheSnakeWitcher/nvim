41e60b5ae782d8daad2e54c6519d57908241fc96
Mr.PAPI <thesnakewitcher@gmail.com>
2025-08-18 22:01:14 -0300

    use virtual_text and virtual_lines in diagnostics

--- lua/diagnostic.lua
+++ lua/diagnostic.lua
@@ -2,6 +2,19 @@ local icons = require("util.icons")
 
 ---@help {diagnostic.txt}
 vim.diagnostic.config({
+    virtual_text = {
+         current_line = true,
+         severity = {
+             min = vim.diagnostic.severity.INFO,
+             max = vim.diagnostic.severity.WARN,
+         }
+    },
+
+    virtual_lines = {
+         current_line = true,
+         severity = { min = vim.diagnostic.severity.ERROR }
+     },
+
     signs = {
         text = {
             [vim.diagnostic.severity.ERROR] = icons.DiagnosticError,
