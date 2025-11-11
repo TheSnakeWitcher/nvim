f5edf16e23edb78ffa2ba99b9efdc1504378d8ec
Mr.PAPI <thesnakewitcher@gmail.com>
2025-11-11 16:20:33 -0300

    change urlview config

--- lua/plugins/urlview.lua
+++ lua/plugins/urlview.lua
@@ -4,17 +4,10 @@ if not status_ok then
     return
 end
 
-
 --- @help {urlview.config}
 urlview.setup({
-
-    default_title = "Links:",
     default_picker = "telescope",
-    default_prefix = "https://",
     default_action = "system",
-    unique = true,
-    sorted = true,
-    log_level_min = vim.log.levels.INFO,
     jump = {
         prev = "[u",
         next = "]u",
