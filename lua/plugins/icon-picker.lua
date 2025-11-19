65c42016bca26544c4e3b8329969aafe63ba5e73
Mr.PAPI <thesnakewitcher@gmail.com>
2025-11-18 22:09:52 -0300

    remove icon-picker config

--- lua/plugins/icon-picker.lua
+++
@@ -1,9 +0,0 @@
-local ok, icon_picker = pcall(require,"icon-picker")
-if not ok then
-    vim.notify "icon-picker config not loaded"
-    return
-end
-
-icon_picker.setup({
-    disable_legacy_commands = true
-})
