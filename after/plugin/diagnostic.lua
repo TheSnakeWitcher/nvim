fdf1077f863c5554d0ac494ff7d1b2e20e4c661d
Mr.PAPI <thesnakewitcher@gmail.com>
2025-12-01 13:01:22 -0300

    move dap sign definition

--- after/plugin/diagnostic.lua
+++ after/plugin/diagnostic.lua
@@ -24,8 +24,3 @@ vim.diagnostic.config({
         },
     }
 })
-
----@help {signs}
-vim.fn.sign_define("DapBreakpoint", { text = icons.DapBreakpoint, texthl = "DapBreakpoint" })
-vim.fn.sign_define("DapBreakpointCondition", { text = icons.DapBreakpointCondition, texthl = "DapBreakpointCondition" })
-vim.fn.sign_define("DapBreakpointRejected", { text = icons.DapBreakpointRejected, texthl = "DapBreakpointRejected" })
--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -15,20 +15,8 @@ if not ok then
 end
 
 --- @help {nvim-dap-virtual-text}
-nvim_dap_virtual_text.setup({
-	enabled = true,
-	enabled_commands = true,
-	highlight_changed_variables = true,
-	highlight_new_as_changed = false,
-	show_stop_reason = true,
-	commented = false,
-	only_first_definition = true,
-	all_references = false,
-	display_callback = function(variable, _buf, _stackframe, _node)
-		return variable.name .. " = " .. variable.value
-	end,
-	virt_text_pos = "eol",
-	all_frames = false,
-	virt_lines = false,
-	virt_text_win_col = nil,
-})
+nvim_dap_virtual_text.setup()
+
+vim.fn.sign_define("DapBreakpoint", { text = icons.DapBreakpoint, texthl = "DapBreakpoint" })
+vim.fn.sign_define("DapBreakpointCondition", { text = icons.DapBreakpointCondition, texthl = "DapBreakpointCondition" })
+vim.fn.sign_define("DapBreakpointRejected", { text = icons.DapBreakpointRejected, texthl = "DapBreakpointRejected" })
