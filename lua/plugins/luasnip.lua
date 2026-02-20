42149df27c40cf63cd0801600a68397292aec96c
Mr.PAPI <thesnakewitcher@gmail.com>
2026-02-20 08:05:13 -0300

    refactor luasnip config

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,10 +4,6 @@ if not ok then
     return
 end
 
-local types = require("luasnip.util.types")
-local lua_loader = require("luasnip.loaders.from_lua") --- @help {luasnip-loaders}
-
-
 --- @help {luasnip-config-options}
 ls.config.set_config({
 
@@ -16,7 +12,7 @@ ls.config.set_config({
 
     --- @help {luasnip-ext_opts}
     ext_opts = {
-        [types.choiceNode] = {
+        [require("luasnip.util.types").choiceNode] = {
           active = {
             virt_text = { { '●', 'DiagnosticHint' } },
           },
@@ -24,7 +20,8 @@ ls.config.set_config({
     },
 })
 
-lua_loader.lazy_load({ path = vim.g.path.snippets })
+--- @help {luasnip-loaders}
+require("luasnip.loaders.from_lua").lazy_load({ path = vim.g.path.snippets })
 ls.filetype_extend("typescriptreact", { "typescript" })
 
 vim.api.nvim_create_user_command(
@@ -32,3 +29,22 @@ vim.api.nvim_create_user_command(
     require("luasnip.loaders").edit_snippet_files,
     {desc = "edit filetype snippets"}
 )
+
+vim.keymap.set({"i"}, "<C-Y>", function() ls.expand() end, {silent = true})
+
+-- vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+-- vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-n>", function()
+	if ls.choice_active() then ls.change_choice(1) end
+end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-p>", function()
+	if ls.choice_active() then ls.change_choice(-1) end
+end, {silent = true})
+
+vim.keymap.set({"i","s"},"<C-s>",function()
+    if ls.choice_active() then
+        require('luasnip.extras.select_choice')()
+    end
+end,{desc = "select choice" , silent = true})
