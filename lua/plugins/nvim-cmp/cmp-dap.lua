3d6d3ee25cd4dc5c3c020160554f1f967f90d64f
Mr.PAPI <thesnakewitcher@gmail.com>
2026-02-20 16:23:20 -0300

    remove old nvim-cmp plugin config

--- lua/plugins/nvim-cmp/cmp-dap.lua
+++
@@ -1,7 +0,0 @@
-local cmp = require("cmp")
-
-cmp.setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
-    sources = cmp.config.sources({
-        { name = "dap" },
-    }),
-})
--- lua/plugins/nvim-cmp/cmp-otter.lua
+++
@@ -1,8 +0,0 @@
-local cmp = require("cmp")
-
---- @help {cmp.setup.filetype}
-cmp.setup.filetype({'markdown'}, {
-    sources = cmp.config.sources({
-        { name = 'otter' },
-    })
-})
